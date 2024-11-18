5fb059a933a039fb8516d937613e781d68d646b9
shahab29 <shahabsabir7@gmail.com>
2024-11-18 20:14:59 +0500

    js code write

--- script.js
+++ script.js
@@ -4,5 +4,7 @@
 
 document.querySelector('.message').textContent = 'start guessing...!';
 
+console.log(document.querySelector('.center').textContent );
+
 document.querySelector('.center').textContent = 16;
 
