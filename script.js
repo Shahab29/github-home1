2efcbf04adef3e7211b1a45c031b43e1819fc2c4
shahab29 <shahabsabir7@gmail.com>
2024-11-20 16:38:26 +0500

    html or css or js code add

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-'use strict';
 
-
-
-document.querySelector('.message').textContent = 'start guessing...!';
-
-console.log(document.querySelector('.center').textContent );
-
-document.querySelector('.center').textContent = 16;
 
